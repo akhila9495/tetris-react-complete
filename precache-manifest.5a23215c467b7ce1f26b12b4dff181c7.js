4e34cc9a9cc5ef1a009c93be3d01d3643b8f746e
Akhil A <37975716+akhila9495@users.noreply.github.com>
2022-08-30 19:41:46 +0530

    Add files via upload

---
+++ precache-manifest.5a23215c467b7ce1f26b12b4dff181c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcda065aa0b73bc3b514670604bf81de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c84ef06b7229dcd4a6a",
+    "url": "/static/css/main.4a04c5eb.chunk.css"
+  },
+  {
+    "revision": "70dc76cbf98e62ed6634",
+    "url": "/static/js/2.635ce553.chunk.js"
+  },
+  {
+    "revision": "0c84ef06b7229dcd4a6a",
+    "url": "/static/js/main.687f02fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/static/media/bg.2810fe6b.png"
+  }
+]);
